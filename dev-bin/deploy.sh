da74165a5d54e2f33eb4816e0a0c84972d6f9fe2
ethan brooks <ebrooks@hotelherrera.com>
2019-08-06 11:19:48 -0500

    aot optimize

--- dev-bin/deploy.sh
+++ dev-bin/deploy.sh
@@ -21,7 +21,7 @@ aws s3 rm s3://${S3BUCKETNAME} --recursive  --profile pp
 #Sync everything EXCEPT *.js, *.css and index.html 
 # js&css were gzipped so need content-encoding explictly set to gzip 
 # index.html CANT be cached at the edge 
-aws s3 sync dist/browser s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "index.html" --exclude "*.css" --exclude "*.js" || { echo 'ERROR: s3 sync failed' ; exit 1; } 
+aws s3 sync dist/browser s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "index.html" --exclude "*.css" --exclude "*.map" --exclude "*.js" || { echo 'ERROR: s3 sync failed' ; exit 1; } 
  
 # Sync js&css 
 aws s3 sync dist/browser s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "*" --include "*.css" --include "*.js" || { echo 'ERROR: s3 js/css sync failed' ; exit 1; } 
