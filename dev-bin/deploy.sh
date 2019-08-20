f414852f365468fec333d3af4848bfec230b3d3a
ethan brooks <ebrooks@hotelherrera.com>
2019-08-19 22:48:58 -0500

    cordova

--- dev-bin/deploy.sh
+++ dev-bin/deploy.sh
@@ -14,17 +14,17 @@ else
     exit 1
 fi
 
-git rev-parse HEAD > platforms/browser/current-git-commit.txt || { echo 'ERROR: git hash failed' ; exit 1; }
+git rev-parse HEAD > www/current-git-commit.txt || { echo 'ERROR: git hash failed' ; exit 1; }
 #for old in www/*.gz; do mv $old ${old%%.gz*}; done
 #for old in www/assets/css/*.gz; do mv $old ${old%%.gz*}; done
 aws s3 rm s3://${S3BUCKETNAME} --recursive  --profile pp
 #Sync everything EXCEPT *.js, *.css and index.html 
 # js&css were gzipped so need content-encoding explictly set to gzip 
 # index.html CANT be cached at the edge 
-aws s3 sync platforms/browser s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "index.html" --exclude "*.css" --exclude "*.map" --exclude "*.js" || { echo 'ERROR: s3 sync failed' ; exit 1; } 
+aws s3 sync www s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "index.html" --exclude "*.css" --exclude "*.map" --exclude "*.js" || { echo 'ERROR: s3 sync failed' ; exit 1; } 
  
 # Sync js&css 
-aws s3 sync platforms/browser s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "*" --include "*.css" --include "*.js" || { echo 'ERROR: s3 js/css sync failed' ; exit 1; } 
+aws s3 sync www s3://${S3BUCKETNAME} --profile pp --metadata-directive REPLACE --cache-control max-age=31536000,public --acl public-read --exclude "*" --include "*.css" --include "*.js" || { echo 'ERROR: s3 js/css sync failed' ; exit 1; } 
  
 # copy index.html making sure its not cached at the edge 
-aws s3 cp platforms/browser/index.html s3://${S3BUCKETNAME}/index.html --profile pp --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read || { echo 'ERROR: s3 cp index failed' ; exit 1; } 
+aws s3 cp www/index.html s3://${S3BUCKETNAME}/index.html --profile pp --metadata-directive REPLACE --cache-control max-age=0,no-cache,no-store,must-revalidate --content-type text/html --acl public-read || { echo 'ERROR: s3 cp index failed' ; exit 1; } 
